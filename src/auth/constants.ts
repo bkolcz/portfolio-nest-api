a560c50145c676987959d68ea1bee50c1e91cd54
Bartłomiej Kołcz <bkolcz@bartisoft.com>
2023-06-02 09:34:41 +0200

    Descriptions
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      modified:   README.md
      modified:   package.json
      modified:   src/auth/constants.ts

--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -1,3 +1,4 @@
 export const jwtConstants = {
+    // In non presentation code do as following, here it's left on purpose.
     secret: 'DO NOT USE THIS VALUE. INSTEAD, CREATE A COMPLEX SECRET AND KEEP IT SAFE OUTSIDE OF THE SOURCE CODE.',
 };
